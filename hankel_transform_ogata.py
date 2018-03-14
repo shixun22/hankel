1d5295997bf40155be4e27e519408aeca027c85b
shixun22 <x.straybird@gmail.com>
2018-03-14 13:30:59 +0100

    Update hankel_transform_ogata.py

--- hankel_transform_ogata.py
+++ hankel_transform_ogata.py
@@ -21,24 +21,24 @@ from numpy import pi,sinh,cosh,tanh
 
 
 def DEtransform(t):
-    return t*tanh(pi/2.*sinh(t))
+    return t * tanh(pi / 2. * sinh(t))
 
 def deriv_DEtransform(t):
-    res = 1./cosh(pi/2.*sinh(t))
-    res = pi/2.*t*cosh(t)*res*res + tanh(pi/2.*sinh(t))
+    res = 1. / cosh(pi / 2. * sinh(t))
+    res = pi / 2. * t * cosh(t) * res * res + tanh(pi / 2. * sinh(t))
     return res
 
-def Descrete_Hankel_Transfrom(x,ff, phase=0,Nroots=50,h=0.01):  
+def Descrete_Hankel_Transfrom(x, ff, phase=0, Nroots=50, h=0.01):  
 # result(x) = int_0^inf  ff(l) J_nv(l*x) ldl
 # 1/h: node density; Nroots: # of points to use to approximate the integral
     nv = abs(phase)
-    zeros_PI = jn_zeros(nv,Nroots)/pi
-    phi_dot = deriv_DEtransform(h*zeros_PI)
-    y_k = DEtransform(h*zeros_PI)*pi/h
-    w_nv_k = yn(nv, zeros_PI*pi)/jn(nv+1,zeros_PI*pi)
-    J_nv = jn(nv,y_k)
-    res = pi/x**2*w_nv_k*y_k*ff(y_k/x)*J_nv*phi_dot
-    return res.sum()*np.sign(phase+0.1)**phase  # +0.1 to give 1 for phase=0
+    zeros_PI = jn_zeros(nv,Nroots) / pi
+    phi_dot = deriv_DEtransform(h * zeros_PI)
+    y_k = DEtransform(h*zeros_PI) * pi / h
+    w_nv_k = yn(nv, zeros_PI * pi) / jn(nv + 1, zeros_PI * pi)
+    J_nv = jn(nv, y_k)
+    res = pi / x**2 * w_nv_k * y_k * ff(y_k / x) * J_nv * phi_dot
+    return res.sum() * np.sign(phase + 0.1)**phase  # +0.1 to give 1 for phase=0
 HankelT = vectorize(Descrete_Hankel_Transfrom)
 
 
